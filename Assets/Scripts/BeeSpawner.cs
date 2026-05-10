33d43313f21a898f975fc06d7e37df509a3e8c96
dustgbwixgit config --global user.namegit config --global --add user.name dustgbwixgit config --global --add user.email boysmiron@gmail.com <boysmiron@gmail.com>
2026-05-10 22:23:00 +0100

    Update BeeSpawner.cs

--- Assets/Scripts/BeeSpawner.cs
+++ Assets/Scripts/BeeSpawner.cs
@@ -3,11 +3,16 @@ using UnityEngine;
 public class BeeSpawner : MonoBehaviour
 {
     [SerializeField] private GameObject bee;
+    private bool hasSpawned = false;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            bee.SetActive(true);
+            if (hasSpawned == false)
+            {
+                bee.SetActive(true);
+                hasSpawned = true;
+            }
         }
     }
 }
